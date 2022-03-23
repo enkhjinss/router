d28d6385baa0550b6843ecc474b2913639f29d06
enkhjinss <enkhjinnasanbayar7@gmail.com>
2022-03-23 14:15:24 +0800

    route

---
+++ src/components/Child.js
@@ -0,0 +1,3 @@
+export const Child = () => {
+    return <div className="child"></div>;
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import App from "./components/App";
+import { Child } from "./components/Child";
+import "./index.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                {/* <Route path="/:color:color2" element={<App />} /> */}
+                <Route path="/:color1" element={<App />}>
+                    <Route path=":color2" element={<Child />}></Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
-
