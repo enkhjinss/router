db56e0c0345d24803bb603e69f3e4ab3683c3afa
enkhjinss <enkhjinnasanbayar7@gmail.com>
2022-03-23 16:32:26 +0800

    done router-color

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import App from "./components/App";
-import { Child } from "./components/Child";
 import "./index.css";
 
 ReactDOM.render(
@@ -12,7 +11,9 @@ ReactDOM.render(
             <Routes>
                 {/* <Route path="/:color:color2" element={<App />} /> */}
                 <Route path="/:color1" element={<App />}>
-                    <Route path=":color2" element={<Child />}></Route>
+                    <Route path=":color2" element={<App />}>
+                        <Route path=":color3" element={<App />}></Route>
+                    </Route>
                 </Route>
             </Routes>
         </BrowserRouter>
